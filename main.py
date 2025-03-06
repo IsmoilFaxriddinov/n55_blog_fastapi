4db29199c442f79f023704573df0768afe0bf0df
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-03-06 22:29:43 +0500

    fastapi first commit

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-from starlette.responses import RedirectResponse
-
-from app.router import users, posts
 
 app = FastAPI()
 
-app.include_router(router=users.router)
-app.include_router(router=posts.router)
-
-@app.get('/', include_in_schema=False)
+@app.get('/')
 async def root():
-    return RedirectResponse('docs/')
+    return "Hello world"
